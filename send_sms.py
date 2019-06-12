4f9ede6b1d55a8bf55efc860435919c0b890d861
Thomas Morita <tym479@gmail.com>
2019-06-12 15:36:20 -0500

    successfully sent response payload about weather in fayetteville to phone as smsgit add .

--- send_sms.py
+++ send_sms.py
@@ -9,13 +9,17 @@ account_sid = 'AC7f63b30dd407f399af7d53185d8a3b61'
 auth_token = '52a6f851b2c35f9056142e164b803d81'
 client = Client(account_sid, auth_token)
 
-# message = client.messages \
-#                 .create(
-#                      body="RAIN CHECK - Hey, it looks like it's going to rain today. BRING YOUR UMBRELLA!",
-#                      from_='+14792822715',
-#                      to='+14799258596'
-#                  )
-
+# Get weather info here
 response = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=72701,us&APPID=462542c82e839a074d9eed2a3e8408a8')
-print(response.json())
-# print(message.sid)
+print(response.text)
+
+
+message = client.messages \
+                .create(
+                     body=response.text,
+                     from_='+14792822715',
+                     to='+14799258596'
+                 )
+
+
+print(message.sid)
