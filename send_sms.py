bec74585a7a64642b636b7775472a546edfe6fa4
Thomas Morita <tym479@gmail.com>
2019-06-12 16:11:30 -0500

    extracted necessary info from payload (weather description and temperature)

--- send_sms.py
+++ send_sms.py
@@ -9,17 +9,23 @@ account_sid = 'AC7f63b30dd407f399af7d53185d8a3b61'
 auth_token = '52a6f851b2c35f9056142e164b803d81'
 client = Client(account_sid, auth_token)
 
-# Get weather info here
+### Get weather info here 
 response = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=72701,us&APPID=462542c82e839a074d9eed2a3e8408a8')
-print(response.text)
+# weather description. Convert unicode to string.
+weather_desc = (response.json()['weather'][0]["description"]).encode("utf-8")
+# current temperature. Convert from flaot to string
+temp_kelvin = response.json()['main']["temp"]
+temp_f = str((temp_kelvin - 273.15)* 9/5 + 32 )
+
+print(weather_desc + " " + temp_f)
 
 
-message = client.messages \
-                .create(
-                     body=response.text,
-                     from_='+14792822715',
-                     to='+14799258596'
-                 )
+# message = client.messages \
+#                 .create(
+#                      body=response.json().main,
+#                      from_='+14792822715',
+#                      to='+14799258596'
+#                  )
 
 
-print(message.sid)
+# print(message.sid)
