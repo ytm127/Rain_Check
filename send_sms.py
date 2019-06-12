bcf343e88f27d2984783b2d4d9fa9ce3867e2675
Thomas Morita <tym479@gmail.com>
2019-06-12 15:31:18 -0500

    get request for Fayetteville, AR

--- send_sms.py
+++ send_sms.py
@@ -1,5 +1,6 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 from twilio.rest import Client
+import requests
 
 
 # Your Account Sid and Auth Token from twilio.com/console
@@ -8,11 +9,13 @@ account_sid = 'AC7f63b30dd407f399af7d53185d8a3b61'
 auth_token = '52a6f851b2c35f9056142e164b803d81'
 client = Client(account_sid, auth_token)
 
-message = client.messages \
-                .create(
-                     body="RAIN CHECK - Hey, it looks like it's going to rain today. BRING YOUR UMBRELLA!",
-                     from_='+14792822715',
-                     to='+14799258596'
-                 )
+# message = client.messages \
+#                 .create(
+#                      body="RAIN CHECK - Hey, it looks like it's going to rain today. BRING YOUR UMBRELLA!",
+#                      from_='+14792822715',
+#                      to='+14799258596'
+#                  )
 
-print(message.sid)
+response = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=72701,us&APPID=462542c82e839a074d9eed2a3e8408a8')
+print(response.json())
+# print(message.sid)
