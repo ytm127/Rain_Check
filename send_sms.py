b5c2132585daa20f7385429162b52b76410c21c4
Thomas Morita <tym479@gmail.com>
2019-06-14 13:49:52 -0500

    clean up

--- send_sms.py
+++ send_sms.py
@@ -11,27 +11,6 @@ account_sid = 'AC7f63b30dd407f399af7d53185d8a3b61'
 auth_token = '52a6f851b2c35f9056142e164b803d81'
 client = Client(account_sid, auth_token)
 
-# ### Get weather info here 
-response = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=72701,us&APPID=462542c82e839a074d9eed2a3e8408a8')
-# weather description. Convert unicode to string.
-weather_desc = (response.json()['weather'][0]["description"]).encode("utf-8")
-# current temperature. Convert from flaot to string
-temp_kelvin = response.json()['main']["temp"]
-temp_f = str((temp_kelvin - 273.15)* 9/5 + 32 )
-info = "Right now, it looks like " + weather_desc + " with a temperature of " + temp_f + " degrees fahrenheit."
-# print(info)
-
-# determine final message depending on forecast. CURRENTLY, IT ONLY LOOKS AT THE CURRENT WEATHER! NEED TO LOOK AT THE DAY'S FORECAST
-if "rain" in weather_desc:
-    message = info + '\n' + "It is raining right now. Bring an umbrella."
-else:
-    message = info + '\n' + "It is not raining right now. Enjoy the weather!"
-
-
-
-
-
-
 ### Get weather info here 
 # This GET request gets 6 timestamps at a 3 hour interval (18 hour span)
 response = requests.get('http://api.openweathermap.org/data/2.5/forecast?id=4110486&cnt=6&APPID=462542c82e839a074d9eed2a3e8408a8')
@@ -45,7 +24,6 @@ for i in range(6):
 # final determination
 if rain == True:
     notice = "There is rain in the forecast between now and 11pm! \n\nBRING YOUR UMBRELLA! \n"
-    print('There is rain in the forecast between now and 11pm! \n\nBRING YOUR UMBRELLA!')
 
     message = client.messages \
                 .create(
